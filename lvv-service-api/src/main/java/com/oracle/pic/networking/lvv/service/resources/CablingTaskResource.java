0837aa52b0288eb90003b5c713999db8a8443473
Nina Niu <nina.niu@oracle.com>
2024-05-07 22:04:09 +0000

    Pull request #16: emit metrics in CablingTaskResource to t2
    Merge in NETAUTO/lvv-service from nniu/cabling-task to master
    
    Squashed commit of the following:
    
    commit d4a0e2cafe5362b35396fbb59b4d13d82140bbf0
    Author: Nina <nina.niu@oracle.com>
    Date:   Tue May 7 13:55:31 2024 -0700
    
        fix format
    
    commit c152c9446c2bcdcdeb1a2b87cb8fdfedb6b31a43
    Author: Nina <nina.niu@oracle.com>
    Date:   Tue May 7 13:01:25 2024 -0700
    
        emit metrics to t2

--- lvv-service-api/src/main/java/com/oracle/pic/networking/lvv/service/resources/CablingTaskResource.java
+++ lvv-service-api/src/main/java/com/oracle/pic/networking/lvv/service/resources/CablingTaskResource.java
@@ -1,6 +1,15 @@
 package com.oracle.pic.networking.lvv.service.resources;
 
+import static com.oracle.pic.networking.lvv.service.resources.CablingTaskResource.Metrics.GetCableValidationFailureTask;
+import static com.oracle.pic.networking.lvv.service.resources.CablingTaskResource.Metrics.GetCableValidationFailureTaskFailure;
+import static com.oracle.pic.networking.lvv.service.resources.CablingTaskResource.Metrics.GetCablingTasks;
+import static com.oracle.pic.networking.lvv.service.resources.CablingTaskResource.Metrics.GetCablingTasksFailure;
+import static com.oracle.pic.networking.lvv.service.resources.CablingTaskResource.Metrics.ResolveValidationFailureTask;
+import static com.oracle.pic.networking.lvv.service.resources.CablingTaskResource.Metrics.ResolveValidationFailureTaskFailure;
+
 import com.google.inject.Inject;
+import com.oracle.pic.commons.metrics.MetricsScope;
+import com.oracle.pic.commons.metrics.metrictypes.Timer;
 import com.oracle.pic.identity.authentication.Principal;
 import com.oracle.pic.identity.authorization.sdk.AuthorizationRequest;
 import com.oracle.pic.networking.lvv.service.api.AbstractCablingTasksResource;
@@ -13,7 +22,18 @@ import lombok.Getter;
 
 public class CablingTaskResource extends AbstractCablingTasksResource {
 
+    private static final String METRIC_SCOPE_NAME = "CablingTaskApi";
+
     private CablingTaskService cablingTaskService;
+
+    enum Metrics {
+        GetCablingTasks,
+        GetCablingTasksFailure,
+        ResolveValidationFailureTask,
+        ResolveValidationFailureTaskFailure,
+        GetCableValidationFailureTask,
+        GetCableValidationFailureTaskFailure
+    }
 
     @Context
     @Getter(AccessLevel.PRIVATE)
@@ -32,18 +52,58 @@ public class CablingTaskResource extends AbstractCablingTasksResource {
             String opcRequestId,
             Principal principal,
             AuthorizationRequest authorizationRequest) {
-        return this.cablingTaskService.getCablingTasks(building, block, rackSerialNumber);
+        try (MetricsScope scope = MetricsScope.create(METRIC_SCOPE_NAME)) {
+            Timer timer = scope.timerStart("createProjectStartMillis");
+            try {
+                CablingTaskCollection collection =
+                        this.cablingTaskService.getCablingTasks(building, block, rackSerialNumber);
+                scope.emit(GetCablingTasks, 1);
+                scope.recordSuccess();
+                return collection;
+            } catch (Exception ex) {
+                scope.emit(GetCablingTasksFailure, 1);
+                throw ex;
+            } finally {
+                scope.timerStop(timer);
+            }
+        }
     }
 
     @Override
     public void resolveValidationFailureTask(
             String cablingTaskId, Principal principal, AuthorizationRequest authorizationRequest) {
-        this.cablingTaskService.resolveValidationFailureTask(cablingTaskId);
+        try (MetricsScope scope = MetricsScope.create(METRIC_SCOPE_NAME)) {
+            Timer timer = scope.timerStart("resolveValidationFailureTaskStartMillis");
+            try {
+                this.cablingTaskService.resolveValidationFailureTask(cablingTaskId);
+                scope.emit(ResolveValidationFailureTask, 1);
+                scope.recordSuccess();
+            } catch (Exception ex) {
+                scope.emit(ResolveValidationFailureTaskFailure, 1);
+                throw ex;
+            } finally {
+                scope.timerStop(timer);
+            }
+        }
     }
 
     @Override
     public String getCableValidationFailureTask(
             String cablingTaskId, Principal principal, AuthorizationRequest authorizationRequest) {
-        return this.cablingTaskService.getCableValidationFailureTask(cablingTaskId);
+        try (MetricsScope scope = MetricsScope.create(METRIC_SCOPE_NAME)) {
+            Timer timer = scope.timerStart("createProjectStartMillis");
+            try {
+                String result =
+                        this.cablingTaskService.getCableValidationFailureTask(cablingTaskId);
+                scope.emit(GetCableValidationFailureTask, 1);
+                scope.recordSuccess();
+                return result;
+            } catch (Exception ex) {
+                scope.emit(GetCableValidationFailureTaskFailure, 1);
+                throw ex;
+            } finally {
+                scope.timerStop(timer);
+            }
+        }
     }
 }
